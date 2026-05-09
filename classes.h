70b0d79ba46a438851e90db2cd819d6fd3165317
k250136-ui <k250136@nu.edu.pk>
2026-05-09 12:03:59 +0500

    changes in base

--- classes.h
+++ classes.h
@@ -1,61 +1,60 @@
 #include<iostream>
 #include<string>
+#include<fstream>
 
 using namespace std;
+
+const int MAX_LISTINGS = 100;
+const int MAX_BUYERS = 30;
+const int MAX_SELLERS = 30;
+const int MAX_ADMINS = 5;
+const int MAX_MESSAGES = 200;
+const int MAX_FAVORITES = 20;
 
 class Engine
 {
 private:
-    int engineId;
-    string engineType;
-    double displacement;
-    int horsepower;
-    double torque;
-    int cylinders;
+	int engineId;
+	string engineType;
+	double displacement;
+	int horsepower;
+	double torque;
+	int cylinders;
 
 public:
-    Engine() : engineId(0), engineType("Unknown"), displacement(0),
-               horsepower(0), torque(0), cylinders(0) {}
+	Engine() : engineId(0), engineType("Unknown"), displacement(0), horsepower(0), torque(0), cylinders(0) {}
 
-    Engine(int id, string type, double disp, int hp, double tq, int cyl)
-        : engineId(id), engineType(type), displacement(disp),
-          horsepower(hp), torque(tq), cylinders(cyl) {}
+	Engine(int id, const string &type, double disp, int hp, double tq, int cyl)
+		: engineId(id), engineType(type), displacement(disp), horsepower(hp), torque(tq), cylinders(cyl) {}
 
-    Engine(const Engine &e)
-        : engineId(e.engineId), engineType(e.engineType),
-          displacement(e.displacement), horsepower(e.horsepower),
-          torque(e.torque), cylinders(e.cylinders) {}
+        // No custom copy constructor is needed because Engine only stores simple value members.
 
-    int getEngineId() const { return engineId; }
-    string getEngineType() const { return engineType; }
-    int getHorsepower() const { return horsepower; }
-    int getCylinders() const { return cylinders; }
+	int getEngineId() const { return engineId; }
+	string getEngineType() const { return engineType; }
+	double getDisplacement() const { return displacement; }
+	int getHorsepower() const { return horsepower; }
+	double getTorque() const { return torque; }
+	int getCylinders() const { return cylinders; }
 
-    void updateEngineType(const string &t) { engineType = t; }
-    void updateDisplacement(double d) { displacement = d; }
-    void updateHorsepower(int hp) { horsepower = hp; }
-    void updateTorque(double t) { torque = t; }
-    void updateCylinders(int c) { cylinders = c; }
-
-    string getSpecs() const
-    {
-        return engineType + " | " + to_string((int)displacement) + "cc | " +
-               to_string(horsepower) + "hp | " + to_string((int)torque) + "Nm | " +
-               to_string(cylinders) + " cyl";
-    }
-
-    bool isHighPerformance() const { return horsepower > 200; }
-
-    double calculateEfficiency() const
-    {
-        if (displacement <= 0)
-            return 0;
-        return (double)15000 / displacement;
-    }
-
-    void display() const
-    {
-        cout << "  [Engine #" << engineId << "] " << getSpecs()
-             << " | Est. " << calculateEfficiency() << " km/L\n";
-    }
+	void display() const
+	{
+		cout << "Engine: " << engineType << " " << displacement << "cc | "
+			 << horsepower << "hp | " << torque << "Nm | " << cylinders << " cyl\n";
+	}
 };
+
+class Address
+{
+private:
+	string street, city, province;
+
+public:
+	Address() : street(""), city(""), province("") {}
+	Address(const string &st, const string &ct, const string &pv) : street(st), city(ct), province(pv) {}
+
+	void display() const
+	{
+		cout << street << ", " << city << ", " << province;
+	}
+};
+
